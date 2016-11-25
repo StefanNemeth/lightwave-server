417c9b5258c5ac8e06522a4818eabae070678639
Steve Winfield <mail.stevewinfield@gmail.com>
2016-11-25 22:22:59 +0100

    Create service application base

---
+++ lightwave-service/src/main/scala/de/lightwave/services/ServiceApp.scala
@@ -0,0 +1,53 @@
+package de.lightwave.services
+
+import akka.actor.ActorSystem
+import akka.cluster.Cluster
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.Logger
+
+import scala.collection.JavaConversions._
+
+/**
+  * Services that can be run independently should extend this trait
+  * for the sake of consistency.
+  */
+trait ServiceApp {
+  def main(args: Array[String]): Unit = {
+    val servicePort = args match {
+      case Array()     => "0"
+      case Array(port) => port
+      case _           => throw new IllegalArgumentException(s"only ports. Args [ $args ] are invalid")
+    }
+
+    val properties = Map(
+      "akka.remote.netty.tcp.port" -> servicePort
+    )
+
+    val config = ConfigFactory.parseMap(properties).withFallback(ConfigFactory.load())
+    val system = ActorSystem(ServiceApp.SystemName, config)
+
+    ServiceApp.Log.info(s"Starting [${config.getStringList("akka.cluster.roles").mkString(",")}] service app..")
+    ServiceApp.Log.info("Waiting for cluster member to be 'Up'..")
+
+    val cluster = Cluster(system)
+
+    cluster registerOnMemberUp {
+      ServiceApp.Log.info(s"Service node up on '${cluster.selfAddress}'!")
+      onStart(system)
+    }
+  }
+
+  /**
+    * To be defined by the service implementation.
+    * It's called when the cluster is ready to go.
+    * @param system The current actor system
+    */
+  def onStart(system: ActorSystem): Unit
+}
+
+object ServiceApp {
+  val Log = Logger[ServiceApp]
+
+  // Global actor system name
+  val SystemName = "lightwave"
+}
