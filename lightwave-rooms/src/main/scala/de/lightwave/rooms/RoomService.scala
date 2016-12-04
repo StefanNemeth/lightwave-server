021c51a412146265b90b31d3d888f87dd6acf9e3
Steve Winfield <mail.stevewinfield@gmail.com>
2016-12-04 08:51:26 +0100

    Change proxy object of replicator request from tuple to case obj

--- lightwave-rooms/src/main/scala/de/lightwave/rooms/RoomService.scala
+++ lightwave-rooms/src/main/scala/de/lightwave/rooms/RoomService.scala
@@ -4,7 +4,7 @@ import akka.actor.{Actor, ActorRef, Props}
 import akka.cluster.Cluster
 import akka.cluster.ddata.Replicator._
 import akka.cluster.ddata.{DistributedData, LWWMap, LWWMapKey}
-import de.lightwave.rooms.RoomService.{GetRoom, PutInCache}
+import de.lightwave.rooms.RoomService.{GetRoom, PutInCache, RequestRoom}
 import de.lightwave.rooms.model.Room
 import de.lightwave.rooms.model.Rooms.RoomId
 import de.lightwave.rooms.repository.RoomRepository
@@ -35,22 +35,23 @@ class RoomService(roomRepository: RoomRepository) extends Actor {
   }
 
   override def receive = {
-    case GetRoom(id) => replicator ! Get(DataKey, ReadLocal, Some((id, sender())))
+    case GetRoom(id) => replicator ! Get(DataKey, ReadLocal, Some(RequestRoom(id, sender())))
     case PutInCache(id, room) => replicator ! Update(DataKey, LWWMap.empty[Room], WriteLocal)(_ + (id.toString -> room))
 
-    case g @ GetSuccess(_, Some((roomId: Int, replyTo: ActorRef))) => g.dataValue match {
+    case g @ GetSuccess(_, Some(RequestRoom(roomId, replyTo))) => g.dataValue match {
       case data: LWWMap[_] => data.get(roomId.toString) match {
         case Some(room) => replyTo ! Some(room)
         case None => getRoomById(roomId) pipeTo replyTo
       }
     }
 
-    case NotFound(_, Some((roomId: Int, replyTo: ActorRef))) => getRoomById(roomId) pipeTo replyTo
+    case NotFound(_, Some(RequestRoom(roomId, replyTo))) => getRoomById(roomId) pipeTo replyTo
   }
 }
 
 object RoomService {
   case class GetRoom(id: RoomId)
+  case class RequestRoom(id: RoomId, replyTo: ActorRef)
   case class PutInCache(id: RoomId, room: Room)
 
   // Use Postgres repository by default
