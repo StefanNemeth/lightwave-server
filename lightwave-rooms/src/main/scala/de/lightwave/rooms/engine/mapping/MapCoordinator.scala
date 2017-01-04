3fbc3758d0970668be363628ab9f7901476dee5a
Stefan Nemeth <stefan@Stefans-Air.fritz.box>
2017-01-04 22:18:21 +0100

    Add get and set commands to map coordinator

--- lightwave-rooms/src/main/scala/de/lightwave/rooms/engine/mapping/MapCoordinator.scala
+++ lightwave-rooms/src/main/scala/de/lightwave/rooms/engine/mapping/MapCoordinator.scala
@@ -3,6 +3,7 @@ package de.lightwave.rooms.engine.mapping
 import akka.actor.{ActorLogging, Props}
 import de.lightwave.rooms.engine.EngineComponent
 import de.lightwave.rooms.engine.EngineComponent.{AlreadyInitialized, Initialize, Initialized}
+import de.lightwave.rooms.engine.mapping.MapCoordinator.{GetState, GetHeight, SetStateAndHeight, InitializedFallback}
 import de.lightwave.rooms.model.{RoomModel, RoomModels}
 import de.lightwave.rooms.repository.RoomModelRepository
 
@@ -21,8 +22,7 @@ class MapCoordinator(modelRepository: RoomModelRepository) extends EngineCompone
 
     heights = RoomModelParser.toHeightMap(model)
     states = RoomModelParser.toStateMap(model)
-
-    sender() ! Initialized
+  
     context.become(initializedReceive)
   }
 
@@ -30,20 +30,39 @@ class MapCoordinator(modelRepository: RoomModelRepository) extends EngineCompone
 
   def initialReceive: Receive = {
     case Initialize(room) => room.modelId match {
-      case None => init(RoomModels.DefaultMap)
+      case None =>
+        init(RoomModels.DefaultMap)
+        sender() ! InitializedFallback
       case Some(modelId) => modelRepository.getById(modelId) match {
-        case None => init(RoomModels.DefaultMap)
-        case Some(model) => init(model)
+        case None =>
+          init(RoomModels.DefaultMap)
+          sender() ! InitializedFallback
+        case Some(model) =>
+          init(model)
+          sender() ! Initialized
       }
     }
   }
 
   def initializedReceive: Receive = {
     case Initialize(_) => sender() ! AlreadyInitialized
+
+    case GetState(x, y) => sender() ! states.get(x, y)
+    case GetHeight(x, y) => sender() ! heights.get(x, y)
+
+    case SetStateAndHeight(x, y, state, height) =>
+      states.set(x, y)(state)
+      heights.set(x, y)(height)
   }
 }
 
 object MapCoordinator {
+  case class GetState(x: Int, y: Int)
+  case class GetHeight(x: Int, y: Int)
+  case class SetStateAndHeight(x: Int, y:Int, state: MapUnit, height: Int)
+
+  case object InitializedFallback
+
   def props(modelRepository: RoomModelRepository) = Props(classOf[MapCoordinator], modelRepository)
   def props(): Props = props(RoomModelRepository)
 }
--- lightwave-rooms/src/test/scala/de/lightwave/rooms/engine/mapping/MapCoordinatorSpec.scala
+++ lightwave-rooms/src/test/scala/de/lightwave/rooms/engine/mapping/MapCoordinatorSpec.scala
@@ -3,7 +3,9 @@ package de.lightwave.rooms.engine.mapping
 import akka.actor.{ActorRef, ActorSystem}
 import akka.testkit.{DefaultTimeout, ImplicitSender, TestKit}
 import com.typesafe.config.ConfigFactory
+import de.lightwave.rooms.model.Room
 import de.lightwave.rooms.engine.EngineComponent.{AlreadyInitialized, Initialize, Initialized}
+import de.lightwave.rooms.engine.mapping.MapCoordinator.{InitializedFallback, GetState, GetHeight, SetStateAndHeight}
 import de.lightwave.rooms.repository.{RoomModelRepositorySpec, RoomRepositorySpec}
 import org.scalatest.{BeforeAndAfterAll, FunSuiteLike}
 
@@ -20,6 +22,20 @@ class MapCoordinatorSpec extends TestKit(ActorSystem("test-system", ConfigFactor
     }
   }
 
+  test("Use fallback model when initializing coordinator using no model") {
+    withActor() { coordinator =>
+      coordinator ! Initialize(Room(Some(1), "", "", None))
+      expectMsg(InitializedFallback)
+    }
+  }
+
+  test("Use fallback model when initializing coordinator using non-existent model") {
+    withActor() { coordinator =>
+      coordinator ! Initialize(Room(Some(1), "", "", Some("non_existent")))
+      expectMsg(InitializedFallback)
+    }
+  }
+
   test("Initialize coordinator only once") {
     withActor() { coordinator =>
       coordinator ! Initialize(RoomRepositorySpec.expectedRoom)
@@ -27,6 +43,41 @@ class MapCoordinatorSpec extends TestKit(ActorSystem("test-system", ConfigFactor
 
       coordinator ! Initialize(RoomRepositorySpec.expectedRoom)
       expectMsg(AlreadyInitialized)
+    }
+  }
+
+  test("Get state of map") {
+    withActor() { coordinator =>
+      coordinator ! Initialize(Room(Some(1), "", "", None))
+      expectMsg(InitializedFallback)
+
+      coordinator ! GetState(2, 0)
+      expectMsg(Some(Tile))
+    }
+  }
+
+  test("Get height of map") {
+    withActor() { coordinator =>
+      coordinator ! Initialize(Room(Some(1), "", "", None))
+      expectMsg(InitializedFallback)
+
+      coordinator ! GetHeight(2, 0)
+      expectMsg(Some(0))
+    }
+  }
+
+  test("Set state and height of map") {
+    withActor() { coordinator =>
+      coordinator ! Initialize(Room(Some(1), "", "", None))
+      expectMsg(InitializedFallback)
+
+      coordinator ! SetStateAndHeight(2, 0, Door, 2)
+      
+      coordinator ! GetState(2, 0)
+      expectMsg(Some(Door))
+      
+      coordinator ! GetHeight(2, 0)
+      expectMsg(Some(2))
     }
   }
 
