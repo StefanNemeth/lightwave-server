18356134a7188fdca8729b965c60c2b9c4638626
Steve Winfield <mail.stevewinfield@gmail.com>
2017-01-06 23:29:11 +0100

    Add compatible database service to docker compose file

---
+++ scripts/init-postgres-db.sql
@@ -0,0 +1,12 @@
+CREATE TABLE room_model (
+	id varchar(25) NOT NULL PRIMARY KEY,
+	height_map text NOT NULL,
+	door_position varchar(9) NOT NULL
+);
+
+CREATE TABLE room (
+	id integer NOT NULL PRIMARY KEY,
+	name varchar(25) NOT NULL,
+	description varchar(128) NOT NULL,
+	model_id varchar(25) NOT NULL REFERENCES room_model
+);
