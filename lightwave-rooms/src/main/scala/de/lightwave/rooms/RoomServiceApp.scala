766facd3c7d5ab67a32813dcc7766d217aae1590
Steve Winfield <mail.stevewinfield@gmail.com>
2017-01-26 21:57:23 +0100

    Add help command for room service app

--- lightwave-rooms/src/main/scala/de/lightwave/rooms/RoomServiceApp.scala
+++ lightwave-rooms/src/main/scala/de/lightwave/rooms/RoomServiceApp.scala
@@ -1,14 +1,24 @@
 package de.lightwave.rooms
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorRef, ActorSystem}
 import com.typesafe.config.Config
-import de.lightwave.dedicated.commands.DedicatedServerCommandHandler
+import de.lightwave.dedicated.commands.{DedicatedServerCommandContext, DedicatedServerCommandHandler}
+import de.lightwave.rooms.engine.RoomEngine.InitializeRoom
+import de.lightwave.rooms.model.Room
 import de.lightwave.services.ServiceApp
+
+object DedicatedRoomServiceCommandContext extends DedicatedServerCommandContext {
+  override def handle(args: Array[String]) = {
+    case "help" => write("Available commands:")
+  }
+}
 
 object RoomServiceApp extends ServiceApp {
   val ServiceName = "roomService"
 
   override def onStart(config: Config, system: ActorSystem, commandHandler: DedicatedServerCommandHandler) = {
     system.actorOf(RoomService.props(), ServiceName)
+
+    commandHandler.setContext(DedicatedRoomServiceCommandContext)
   }
 }
