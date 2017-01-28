15e8137ac16a773a944d9ea1520f6226b085132b
Steve Winfield <mail.stevewinfield@gmail.com>
2017-01-28 22:16:08 +0100

    Enable room sharding

--- lightwave-rooms/src/main/scala/de/lightwave/rooms/RoomServiceApp.scala
+++ lightwave-rooms/src/main/scala/de/lightwave/rooms/RoomServiceApp.scala
@@ -1,9 +1,12 @@
 package de.lightwave.rooms
 
 import akka.actor.{ActorRef, ActorSystem}
+import akka.cluster.sharding.{ClusterSharding, ClusterShardingSettings}
 import com.typesafe.config.Config
 import de.lightwave.dedicated.commands.{DedicatedServerCommandContext, DedicatedServerCommandHandler}
+import de.lightwave.rooms.engine.RoomEngine
 import de.lightwave.rooms.engine.RoomEngine.InitializeRoom
+import de.lightwave.rooms.engine.entities.RoomEntity
 import de.lightwave.rooms.model.Room
 import de.lightwave.services.ServiceApp
 
@@ -17,6 +20,15 @@ object RoomServiceApp extends ServiceApp {
   val ServiceName = "roomService"
 
   override def onStart(config: Config, system: ActorSystem, commandHandler: DedicatedServerCommandHandler) = {
+    // Hosts room engines on service (access them via "startProxy")
+    ClusterSharding(system).start(
+      typeName = RoomEngine.shardName,
+      entityProps = RoomEngine.props(),
+      settings = ClusterShardingSettings(system),
+      extractEntityId = RoomEngine.extractEntityId,
+      extractShardId = RoomEngine.extractShardId)
+
+    // Provides room data
     system.actorOf(RoomService.props(), ServiceName)
 
     commandHandler.setContext(DedicatedRoomServiceCommandContext)
