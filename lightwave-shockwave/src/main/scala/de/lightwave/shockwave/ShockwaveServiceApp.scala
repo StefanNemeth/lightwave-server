77fe8e5fe104f86a0405568f94dbe53258acd85a
Steve Winfield <mail.stevewinfield@gmail.com>
2017-02-11 13:42:15 +0100

    Add configuration of shockwave server endpoint

--- lightwave-shockwave/src/main/scala/de/lightwave/shockwave/ShockwaveServiceApp.scala
+++ lightwave-shockwave/src/main/scala/de/lightwave/shockwave/ShockwaveServiceApp.scala
@@ -8,7 +8,12 @@ import de.lightwave.dedicated.commands.DedicatedServerCommandHandler
 import de.lightwave.services.ServiceApp
 
 object ShockwaveServiceApp extends ServiceApp {
+  val ConfigEndpointHostPath = "lightwave.shockwave.endpoint.host"
+  val ConfigEndpointPortPath = "lightwave.shockwave.endpoint.port"
+
   override def onStart(config: Config, system: ActorSystem, commandHandler: DedicatedServerCommandHandler): Unit = {
-    system.actorOf(ShockwaveService.props(new InetSocketAddress("127.0.0.1", 30000)), "ShockwaveService")
+    system.actorOf(ShockwaveService.props(
+      new InetSocketAddress(config.getString(ConfigEndpointHostPath), config.getInt(ConfigEndpointPortPath))
+    ), "ShockwaveService")
   }
 }
